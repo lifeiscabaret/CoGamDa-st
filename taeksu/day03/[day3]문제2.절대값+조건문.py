1e26e5cadcf1889c37d104ba58c4c91c0dc27b17
Taek-Su Kim <taeksu880@gmail.com>
2025-12-30 06:12:02 +0900

    수정

--- taeksu/day03/[day3]문제2.절대값+조건문.py
+++
@@ -1,6 +0,0 @@
-a = int(input("정수를 입력하시오: "))
-
-if abs(a) >= 10:
-    print("크다")
-else:
-    print("작다")
--- taeksu/day03/[day3]응용문제2.반올림점수판별.py
+++
@@ -1,6 +0,0 @@
-a = float(input("실수를 입력하시오: "))
-
-if round(a) >= 90:
-    print("합격")
-else:
-    print("불합격")
