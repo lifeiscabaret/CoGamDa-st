e6b749c11be630d360fe703117a0e87e7ab1bfaa
Taek-Su Kim <taeksu880@gmail.com>
2025-12-30 06:40:06 +0900

    day5 풀이 추가

---
+++ taeksu/day05/[day5]심화문제.실전사고문제.py
@@ -0,0 +1,7 @@
+a = int(input("정수를 입력하시오: "))
+b = abs(a)
+
+if b >= 5:
+    print("범위 초과")
+else:
+    print("정상 범위")
---
+++ taeksu/day05/[day5]응용문제1.코드간결과.py
@@ -0,0 +1,27 @@
+# a = int(input())
+# if a > 0:
+#     result = a + 5
+#     print(result)
+# else:
+#     result = a + 5
+#     print(result)
+
+# --------------------
+a = int(input())
+result = a + 5
+
+if a > 0:
+    print(result)
+else:
+    print(result)
+    
+# --------------------
+a = int(input())
+result = a + 5
+
+if a > 0:
+    result = a + 5
+else:
+    result = a + 5
+
+print(result)
