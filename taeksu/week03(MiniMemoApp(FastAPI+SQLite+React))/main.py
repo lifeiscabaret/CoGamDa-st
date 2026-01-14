4152733a2b16dce2d2a261fcede77cdb23b8d85c
Taek-Su Kim <taeksu880@gmail.com>
2026-01-14 23:09:23 +0900

    week03 - taeksu

--- taeksu/week03(MiniMemoApp(FastAPI+SQLite+React))/main.py
+++ taeksu/week03(MiniMemoApp(FastAPI+SQLite+React))/main.py
@@ -1,6 +1,7 @@
 import sqlite3
 from fastapi import FastAPI
 from fastapi import HTTPException
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
@@ -12,11 +13,26 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row # 이 줄이 아주 중요한 힌트입니다!
     return conn
 
+@app.get("/")
+async def get_index():
+    return FileResponse('index.html')
+
 @app.get("/memos")
 async def read_memo():
     conn = get_db_connection()
     rows = conn.execute("SELECT * FROM memos ORDER BY memo_id DESC").fetchall()
     return [dict(row) for row in rows]
+
+@app.put("/memos/{memo_id}")
+async def update_memo(memo_id: int, title: str, content: str = ""):
+    conn = get_db_connection()
+    # SQL 실행: 특정 ID의 제목과 내용을 수정합니다.
+    conn.execute(
+        "UPDATE memos SET title = ?, content = ? WHERE memo_id = ?",
+        (title, content, memo_id)
+    )
+    conn.commit()
+    return {"message": "수정 성공"}
 
 @app.post("/memos")
 async def create_memo(title: str, content: str = ""):
@@ -27,7 +43,7 @@ async def create_memo(title: str, content: str = ""):
     
     if not title.strip():
         raise HTTPException(status_code=400, detail="TITLEREQUIRED") 
-    
+  
     # SQL 명령 실행
     cursor.execute(
         "INSERT INTO memos (title, content) VALUES (?, ?)", (title, content)
@@ -41,6 +57,13 @@ async def create_memo(title: str, content: str = ""):
 
     return {"memo_id": new_id, "title": title, "content": content}
 
+@app.delete("/memos/{memo_id}")
+async def delete_memo(memo_id: int):
+    conn = get_db_connection()
+    # SQL 명령: memos 테이블에서 memo_id가 일치하는 행을 지워라!
+    conn.execute("DELETE FROM memos WHERE memo_id = ?", (memo_id,))
+    conn.commit()
+    return {"message": "삭제 성공"}
 
 # 1. 연결 통로 열기
 conn = sqlite3.connect("app.db")
